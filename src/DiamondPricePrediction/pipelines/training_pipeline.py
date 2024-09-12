d7fd3af31bfb0ae1d9071ce9f4e8d396055d4b5b
omii1809 <137719427+omii1809@users.noreply.github.com>
2024-09-12 14:42:06 +0530

    model training done

--- src/DiamondPricePrediction/pipelines/training_pipeline.py
+++ src/DiamondPricePrediction/pipelines/training_pipeline.py
@@ -1,11 +1,22 @@
 from src.DiamondPricePrediction.components.data_ingestion import data_ingestion
-
+from src.DiamondPricePrediction.components.data_transformation import DataTransformation
+from src.DiamondPricePrediction.components.model_trainer import ModelTrainer
 import os
 import sys
 from src.DiamondPricePrediction.logger import logging
 from src.DiamondPricePrediction.exception import custom_exception
 import pandas as pd 
 
+
 obj=data_ingestion()
 
-obj.intiate_data_ingestion()
+train_data_path,test_data_path=obj.intiate_data_ingestion()
+
+data_transform_obj=DataTransformation()
+
+train_arr,test_arr=data_transform_obj.initialize_data_transformation(train_data_path,test_data_path)
+
+model_trainer_obj=ModelTrainer()
+
+model_trainer_obj.initate_model_trainig(train_arr,test_arr)
+    
