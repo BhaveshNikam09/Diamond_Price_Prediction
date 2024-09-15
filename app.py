1e101c45d6f1f2f2c28de5fb3254be48c7d14fa7
omii1809 <137719427+omii1809@users.noreply.github.com>
2024-09-15 22:42:06 +0530

    done changes

--- app.py
+++ app.py
@@ -5,12 +5,7 @@ from flask import Flask,request,render_template,jsonify
 app=Flask(__name__)
 
 
-@app.route('/')
-def home_page():
-    return render_template("index.html")
-
-
-@app.route("/predict",methods=["GET","POST"])
+@app.route("/",methods=["GET","POST"])
 def predict_data():
     if request.method == "GET":
         return render_template("form.html")
