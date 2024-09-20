1e42ddb2c7272804314896f31ec03bd9b91076dc
omii1809 <137719427+omii1809@users.noreply.github.com>
2024-09-20 11:08:04 +0530

    updated with dagshub

--- src/DiamondPricePrediction/components/model_evaluation.py
+++ src/DiamondPricePrediction/components/model_evaluation.py
@@ -31,7 +31,7 @@ class ModelEvaluation:
 
         
 
-            mlflow.set_registry_uri("https://dagshub.com/sunny.savita/fsdsmendtoend.mlflow")
+            mlflow.set_registry_uri("https://dagshub.com/BhaveshNikam09/Diamond_Price_Prediction.mlflow")
             
             tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
             
@@ -53,10 +53,6 @@ class ModelEvaluation:
                 # Model registry does not work with file store
                 if tracking_url_type_store != "file":
 
-                    # Register the model
-                    # There are other ways to use the Model Registry, which depends on the use case,
-                    # please refer to the doc for more information:
-                    # https://mlflow.org/docs/latest/model-registry.html#api-workflow
                     mlflow.sklearn.log_model(model, "model", registered_model_name="ml_model")
                 else:
                     mlflow.sklearn.log_model(model, "model")
